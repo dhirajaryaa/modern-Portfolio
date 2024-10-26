285a1d1e5f9b178f48e00af9f84090c01896c041
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2024-10-26 07:17:52 +0530

    feat: adding font family and adding custom screen size

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        serif: ['Calistoga', 'serif'],
+      },
+      screens: {
+        sm: '375px',
+        md: '768px',
+        lg: '1200px',
+      },
+    },
   },
   plugins: [],
 }
