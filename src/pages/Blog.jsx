6fb3ed5ea865728d21e0f9a57c759e6bc746ae63
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-08-09 09:13:40 +0000

    feat: add read more btn

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { FaSpinner } from "react-icons/fa";
+import { RiExternalLinkLine } from "react-icons/ri";
 import BlogCard from "../components/BlogCard";
 
 function Blog() {
   const [blogs, setBlogs] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState("");
-  const api = "https://dev.to/api/articles?username=dhirajarya01";
+  const api = "https://dev.to/api/articles?username=dhirajarya01&per_page=5";
 
   useEffect(() => {
     const fetchBlogs = async () => {
@@ -27,6 +28,8 @@ function Blog() {
     fetchBlogs();
   }, [api]);
 
+  console.log(blogs);
+
   return (
     <section className="flex flex-col gap-2 my-5 px-3">
       <h1 className="text-lg sm:text-xl font-semibold">my blog 📃:</h1>
@@ -42,6 +45,14 @@ function Blog() {
       ) : (
         <p className="text-sm my-16 text-center">⚠️ No Blog Posts.</p>
       )}
+      {/* more blogs  */}
+      <a
+        href={`https://dev.to/dhirajarya01`}
+        target="_black"
+        className="bg-violet-600 hover:bg-violet-800 duration-200 mx-auto mt-8 w-30 ease-in-out hover:underline text-white flex gap-1 items-center p-2 rounded-lg text-sm font-text"
+      >
+        Read more <RiExternalLinkLine size={18}/>
+      </a>
     </section>
   );
 }
