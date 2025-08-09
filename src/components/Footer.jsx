aa0e83db6682c67a9ad0719329d258aed36ce495
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-08-09 03:19:28 +0000

    style: enhance footer opacity, add hover effect to header links, and improve button styles in Skills component for better interactivity

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
       id="footer"
       aria-label="Footer"
       className="flex z-50 justify-between items-center  text-sm max-w-3xl w-full mx-auto 
-      py-10 text-gray-700 flex-wrap px-2 sm:px-4 gap-2"
+      py-10 flex-wrap px-2 sm:px-4 gap-2 opacity-50"
     >
       <p>© {new Date().getFullYear()} DhirajArya.</p>
       <p>💡Last updated on {lastUpdate}</p>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ function Header({ setTheme,theme }) {
     <header className="flex w-full z-50 justify-center items-center py-4 bg-white text-black dark:bg-slate-950 duration-500 transition-colors dark:text-white sticky top-0">
       <nav className=" flex items-center justify-between max-w-3xl w-full px-2 sm:px-4">
         <NavLink
-          className={`sm:text-3xl text-xl font-semibold cursor-pointer tracking-tight`}
+          className={`sm:text-3xl text-xl font-semibold cursor-pointer tracking-tight hover:underline`}
           to={"/"}
         >
           Dhiraj.dev
@@ -37,7 +37,7 @@ function Header({ setTheme,theme }) {
             pow
           </NavLink>
           {/* theme toggle  */}
-          <button onClick={toggleTheme}>
+          <button onClick={toggleTheme} className="p-1 rounded-full cursor-pointer">
             {theme === "dark" ? (
               <IoIosSunny className="size-5 sm:size-6" />
             ) : (
